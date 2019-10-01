ea7fc723caf42b1f12729a2309532042dd1306e9
allinvestglobal <53827177+allinvestglobal@users.noreply.github.com>
2019-10-01 12:23:53 +0200

    Add files via upload

---
+++ nvic2.py
@@ -0,0 +1,222 @@
+
+
+
+
+from instapy_cli import client
+
+username = 'todolujovicalvaro'
+password = '20million'
+image = 'b24.png'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+from instapy_cli import client
+
+username = 'todolujovicalvaro'
+password = '20million'
+image = 'b25.png'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'todolujovicalvaro'
+password = '20million'
+image = 'b27.png'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'todolujovicalvaro'
+password = '20million'
+image = 'b28.png'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'todolujovicalvaro'
+password = '20million'
+image = 'b29.png'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'todolujovicalvaro'
+password = '20million'
+image = 'b40.png'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'todolujovicalvaro'
+password = '20million'
+image = 'b41.png'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'todolujovicalvaro'
+password = '20million'
+image = 'b42.png'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'todolujovicalvaro'
+password = '20million'
+image = 'b43.png'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'todolujovicalvaro'
+password = '20million'
+image = 'b44.png'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+from instapy_cli import client
+
+username = 'todolujovicalvaro'
+password = '20million'
+image = 'b45.png'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'todolujovicalvaro'
+password = '20million'
+image = 'b46.png'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+from instapy_cli import client
+
+username = 'todolujovicalvaro'
+password = '20million'
+image = 'b26.png'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'todolujovicalvaro'
+password = '20million'
+image = 'b47.png'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
