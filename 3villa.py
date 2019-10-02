e8a83bb0c2d44b7f6507eef920d656a8a162fded
allinvestglobal <53827177+allinvestglobal@users.noreply.github.com>
2019-10-02 13:32:20 +0200

    Add files via upload

---
+++ 3villa.py
@@ -0,0 +1,86 @@
+
+
+
+from instapy_cli import client
+
+username = 'todolujovillaverde'
+password = '20million'
+image = 'b43.png'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'todolujovillaverde'
+password = '20million'
+image = 'b44.png'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+from instapy_cli import client
+
+username = 'todolujovillaverde'
+password = '20million'
+image = 'b45.png'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'todolujovillaverde'
+password = '20million'
+image = 'b46.png'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+from instapy_cli import client
+
+username = 'todolujovillaverde'
+password = '20million'
+image = 'b26.png'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'todolujovillaverde'
+password = '20million'
+image = 'b47.png'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
